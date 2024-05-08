9f3fad2163772be163c1bd8a25743ee49eacf7f8
jargueso <jargueso@alumnat.copernic.cat>
2024-05-08 14:13:52 +0000

    On branch jose-7-de-mayo  Changes to be committed: 	modified:   css/iniciar.css 	modified:   css/registrar.css 	modified:   html/iniciar.html 	modified:   html/registrar.html 	modified:   php/conexion.php 	modified:   php/inicio_registro.php

---
+++ php/conexion.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $conexión = mysqli_connect("localhost", "root", "", "login_register_db" ); /*Cambiar "root" por el nombre de usuario que permite acceder a la base de datos, cambiar las comillas sin texto ("") por la contraseña de usuario si tiene y cambiar "login_register_db" por el nombre de la base de datos */
+    
+    /*if ($conexión){
+        echo 'Conectado';
+    }else{
+        echo 'No se ha podido conectar';
+    } Para saber si funciona o no la conexión*/
+
+
+
+
+?>
