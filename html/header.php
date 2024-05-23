94575a81fdddad40409fd7786318072d39589ede
jargueso <jargueso@alumnat.copernic.cat>
2024-05-23 15:38:01 +0000

    Changes to be committed: 	modified:   html/comunidades.php 	modified:   html/header.php 	modified:   html/iniciar.php 	modified:   html/perfil.php 	modified:   html/registrar.php

--- html/header.php
+++ html/header.php
@@ -5,6 +5,13 @@
     	<link rel="stylesheet" type="text/css" href="../css/header.css">
     </head>
     <body>
-        
+    	<header class="header">
+    		<a href="index.html">INICIO</a>
+            <a href="tendencias.html">TENDENCIAS</a>
+            <a href="hilos.html">HILOS</a>
+            <a href="comunidades.html">COMUNIDADES</a>
+    		<img src="../multimedia/img/a">
+    		<img src="../multimedia/img/perfil.png" class="perfil">
+    	</header>
     </body>
 </html>
--- html/iniciar.php
+++ html/iniciar.php
@@ -17,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio de sesión</title>
     <link rel="stylesheet" href="../css/iniciar.css">
 </head>
 <body>
