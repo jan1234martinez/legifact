e20ff5ad4c4ed4d85d066fa156c5fe35e228953b
jargueso <jargueso@alumnat.copernic.cat>
2024-05-23 15:05:13 +0000

    Changes to be committed: 	modified:   css/hilos.css 	deleted:    html/comunidades.html 	new file:   html/comunidades.php 	new file:   html/header.php 	modified:   html/hilos.html 	modified:   html/perfil.php

---
+++ html/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<link rel="stylesheet" type="text/css" href="../css/header.css">
+    </head>
+    <body>
+    	<header class="header">
+            <title>Legifact</title>
+    		<a href="index.html">INICIO</a>
+            <a href="tendencias.html">TENDENCIAS</a>
+            <a href="hilos.html">HILOS</a>
+            <a href="comunidades.html">COMUNIDADES</a>
+    		<img src="../multimedia/img/a">
+    		<a href="perfil.php" class="perfil_user"><img src="../multimedia/img/perfil.png"></a>
+    	</header>
+    </body>
+</html>
