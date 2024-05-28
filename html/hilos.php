3f471761b9e93eb8f8c287d11646551d83a09bef
jargueso <jargueso@alumnat.copernic.cat>
2024-05-28 14:42:31 +0000

    On branch jose-28-mayo  Changes to be committed: 	modified:   css/hilos.css 	modified:   html/hilos.php

--- html/hilos.php
+++ html/hilos.php
@@ -12,8 +12,20 @@
         <h1 class="titulo">Hilos</h1>
         <div class="buscador_boton">
             <img src="../multimedia/img/triangulo izquierda.png">
-            <input type="text" placeholder="Buscar Hilo">
-            <a href="hilos.html" class="nuevo_hilo">NUEVO HILO</a>
+            <input type="text" placeholder="Buscar Hilo" class="buscador">
+            <a href="nuevo_hilo.html" class="nuevo_hilo">NUEVO HILO</a>
+        </div>
+        <div class="hilos">
+            <img src="../multimedia/img/perfil.png" alt="" class="perfil"></br> <p>Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo</p> <p class="likes">5,3K likes</p> <img src="../multimedia/img/like.png" class="me_gusta">
+        </div>
+        <div class="hilos">
+            <img src="../multimedia/img/perfil.png" alt="" class="perfil"></br> <p>Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo</p> <p class="likes">15K likes</p> <img src="../multimedia/img/like.png" class="me_gusta">
+        </div>
+        <div class="hilos">
+            <img src="../multimedia/img/perfil.png" alt="" class="perfil"></br> <p>Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo</p> <p class="likes">1K likes</p> <img src="../multimedia/img/like.png" class="me_gusta">
+        </div>
+        <div class="hilos">
+            <img src="../multimedia/img/perfil.png" alt="" class="perfil"></br> <p>Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo Titulo Hilo</p> <p class="likes">5K likes</p> <img src="../multimedia/img/like.png" class="me_gusta">
         </div>
     </main>
 </body>
