228081fa86f26705ffbead66f46e279ebf2b920b
jan1234martinez <168751801+jan1234martinez@users.noreply.github.com>
2024-05-28 20:07:38 +0000

    Changes to be committed: 	modified:   css/nuevo_hilo.css 	modified:   html/iniciar.php 	modified:   html/nuevo_hilo.php 	modified:   html/registrar.php 	modified:   php/conexion.php 	new file:   php/publicar_hilo.php

--- html/nuevo_hilo.php
+++ html/nuevo_hilo.php
@@ -1,43 +1,44 @@
+<?php
+    
+    session_start();
+
+    include 'conexion.php';
+    if(!isset($_SESSION['usuario'])){
+        echo '
+            <script>
+                alert("Debes iniciar sesión para crear un hilo");
+                window.location = "iniciar.html;
+            </script>
+        ';
+        session_destroy();
+        exit();   
+    }
+
+?>
+
 <?php include("header.php"); ?>
 <!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="../css/nuevo_hilo.css">
+        <title> Nuevo hilo </title>
     </head>
     <body>
-    <div class="page-container">
-        
-    </div>
-
-    <div class="titulo">
-        <h1>NUEVO HILO</h1>
-    </div>
-    <br>
-
-    <div class="fondo">
-    <br>
-    
-    <div class="text">
-        <input type="text" placeholder="ESCRIBE TU TÍTULO DE HILO AQUÍ">
-    </div>
-
-    <div class="text">
-        <textarea rows="10" cols="170">
-            ESCRIBA SU HILO AQUÍ
-            </textarea>
-    </div>
-    <br>
-
-    <div class="text">
-        <button class="buton">
-            <img src="../multimedia/img/triangulo izquierda.png" class="triangulo">Publicar<img src="../multimedia/img/triangulo drecha.png" class="triangulo">
-        </button>
-        <img src="../multimedia/img/color de letra.png" class="icono">
-        <img src="../multimedia/img/imagen.png" class="icono">
-        <img src="../multimedia/img/emoji.png" class="icono">
-        <img src="../multimedia/img/negreta.png" class="icono">
-        <img src="../multimedia/img/subraya.png" class="icono">
-    </div>
-
+        <div class="todo">
+            <h1 class="titulo"> Nuevo Hilo </h1>
+            <div class="formulario">
+                <form action="../php/publicar_hilo.php" method="$_POST">
+                    <div>
+                        <label for="" > Título: </label> <br>
+                        <input type="text" placeholder="Escribe el título de tu hilo aquí" name="titulo" required class="contenido-formulario">
+                    </div>
+                    <div>
+                        <label for=""> Contenido: </label> <br>
+                        <textarea type="text" placeholder="Escribe el contenido de tu hilo aquí" name="contenido" rows="10" class="contenido-formulario"></textarea>
+                    </div>           
+                    <button class="publicar"> PUBLICAR </button>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
